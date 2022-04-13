5c6ae8c7707397e4441ab86b5b1e5d3be9ffe70e
Meetjain1999 <87543001+Meetjain1999@users.noreply.github.com>
2022-04-14 01:17:54 +0530

    Meet chnages:

--- index.php
+++ index.php
@@ -42,10 +42,11 @@
  
   function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+    FB.api('/me/accounts', function(response) {
+	console.log(response)
+      /*console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        'Thanks for logging in, ' + response.name + '!';*/
     });
   }
 
